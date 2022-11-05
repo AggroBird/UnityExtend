1ed0d9bf981ec71398abf61a089790275d4a175f
AggroBird <jan@aggrobird.com>
2022-11-05 20:00:37 +0900

    Support for int vectors

--- Editor/ClampedAttributeDrawer.cs
+++ Editor/ClampedAttributeDrawer.cs
@@ -35,32 +35,61 @@ namespace AggroBird.UnityEngineExtend.Editor
                 switch (property.propertyType)
                 {
                     case SerializedPropertyType.Integer:
+                    {
                         property.intValue = Clamp(property.intValue);
-                        break;
+                    }
+                    break;
+                    case SerializedPropertyType.Vector2Int:
+                    {
+                        Vector2Int v2 = property.vector2IntValue;
+                        v2.x = Clamp(v2.x);
+                        v2.y = Clamp(v2.y);
+                        property.vector2IntValue = v2;
+                    }
+                    break;
+                    case SerializedPropertyType.Vector3Int:
+                    {
+                        Vector3Int v3 = property.vector3IntValue;
+                        v3.x = Clamp(v3.x);
+                        v3.y = Clamp(v3.y);
+                        v3.z = Clamp(v3.z);
+                        property.vector3IntValue = v3;
+                    }
+                    break;
+
                     case SerializedPropertyType.Float:
+                    {
                         property.floatValue = Clamp(property.floatValue);
-                        break;
+                    }
+                    break;
                     case SerializedPropertyType.Vector2:
+                    {
                         Vector2 v2 = property.vector2Value;
                         v2.x = Clamp(v2.x);
                         v2.y = Clamp(v2.y);
                         property.vector2Value = v2;
-                        break;
+                    }
+                    break;
                     case SerializedPropertyType.Vector3:
+                    {
                         Vector3 v3 = property.vector3Value;
                         v3.x = Clamp(v3.x);
                         v3.y = Clamp(v3.y);
                         v3.z = Clamp(v3.z);
                         property.vector3Value = v3;
-                        break;
+                    }
+                    break;
                     case SerializedPropertyType.Vector4:
+                    {
                         Vector4 v4 = property.vector4Value;
                         v4.x = Clamp(v4.x);
                         v4.y = Clamp(v4.y);
                         v4.z = Clamp(v4.z);
                         v4.w = Clamp(v4.w);
                         property.vector4Value = v4;
-                        break;
+                    }
+                    break;
+
                     default:
                         EditorGUI.LabelField(position, label.text, "Invalid property type used for Clamped attribute");
                         break;
