b9d958cc9e8076c98c4f581fd411822cec0b2521
AggroBird <jan@aggrobird.com>
2022-12-01 17:00:22 +0900

    Added missing swizzle

--- Runtime/Swizzle.cs
+++ Runtime/Swizzle.cs
@@ -5,30 +5,54 @@ namespace AggroBird.UnityEngineExtend
     // Swizzles for setting values in common structs
     public static class Swizzle
     {
+        public static Vector2 GetXY(this Vector3 vec)
+        {
+            return new Vector2(vec.x, vec.y);
+        }
         public static void SetXY(this ref Vector3 vec, Vector2 xy)
         {
             vec.x = xy.x;
             vec.y = xy.y;
-        }
-        public static void SetYZ(this ref Vector3 vec, Vector2 yz)
-        {
-            vec.y = yz.x;
-            vec.z = yz.y;
         }
         public static void SetXY(this ref Vector3 vec, float x, float y)
         {
             vec.x = x;
             vec.y = y;
         }
+
+        public static Vector2 GetXZ(this Vector3 vec)
+        {
+            return new Vector2(vec.x, vec.z);
+        }
+        public static void SetXZ(this ref Vector3 vec, Vector2 xz)
+        {
+            vec.x = xz.x;
+            vec.z = xz.y;
+        }
         public static void SetXZ(this ref Vector3 vec, float x, float z)
         {
             vec.x = x;
             vec.z = z;
         }
+
+        public static Vector2 GetYZ(this Vector3 vec)
+        {
+            return new Vector2(vec.y, vec.z);
+        }
+        public static void SetYZ(this ref Vector3 vec, Vector2 yz)
+        {
+            vec.y = yz.x;
+            vec.z = yz.y;
+        }
         public static void SetYZ(this ref Vector3 vec, float y, float z)
         {
             vec.y = y;
             vec.z = z;
+        }
+
+        public static Vector3 Horizontal3D(this Vector2 vec)
+        {
+            return new Vector3(vec.x, 0, vec.y);
         }
 
         public static void SetRGB(this ref Color c, float rgb)
@@ -42,24 +66,6 @@ namespace AggroBird.UnityEngineExtend
             c.r = r;
             c.g = g;
             c.b = b;
-        }
-
-        public static Vector2 GetXY(this Vector3 vec)
-        {
-            return new Vector2(vec.x, vec.y);
-        }
-        public static Vector2 GetXZ(this Vector3 vec)
-        {
-            return new Vector2(vec.x, vec.z);
-        }
-        public static Vector2 GetYZ(this Vector3 vec)
-        {
-            return new Vector2(vec.y, vec.z);
-        }
-
-        public static Vector3 Horizontal3D(this Vector2 vec)
-        {
-            return new Vector3(vec.x, 0, vec.y);
         }
 
         public static Rect ToRect(this RectInt rect)
