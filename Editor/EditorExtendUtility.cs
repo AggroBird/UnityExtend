ad1a28a00d4aca0e9efd70ed8029fa95a71a5fbd
AggroBird <jan@aggrobird.com>
2023-01-30 16:23:53 +0900

    Added TryGetFieldInfo

---
+++ Editor/EditorExtendUtility.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Reflection;
+using UnityEditor;
+using UnityObject = UnityEngine.Object;
+
+namespace AggroBird.UnityEngineExtend.Editor
+{
+    public static class EditorExtendUtility
+    {
+        public static bool TryGetFieldInfo(this SerializedProperty property, out FieldInfo fieldInfo)
+        {
+            fieldInfo = null;
+            UnityObject obj = property.serializedObject.targetObject;
+            if (obj)
+            {
+                Type parentType = obj.GetType();
+                foreach (string fieldName in property.propertyPath.Split('.'))
+                {
+                    fieldInfo = parentType.GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                    if (fieldInfo == null) break;
+                    parentType = fieldInfo.FieldType;
+                }
+                return fieldInfo != null;
+            }
+            return false;
+        }
+    }
+}
