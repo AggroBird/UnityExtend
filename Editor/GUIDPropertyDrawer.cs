93416668512246e1e1ea742d04aadb71b54a878b
AggroBird <jan@aggrobird.com>
2023-01-31 18:33:12 +0900

    Added GUID property drawer

---
+++ Editor/GUIDPropertyDrawer.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace AggroBird.UnityEngineExtend.Editor
+{
+    [CustomPropertyDrawer(typeof(GUID))]
+    public class GUIDPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            using (new EditorGUI.DisabledGroupScope(true))
+            {
+                long value0 = property.FindPropertyRelative("value0").longValue;
+                long value1 = property.FindPropertyRelative("value1").longValue;
+                EditorGUI.TextField(position, label, $"{value0:x16}{value1:x16}");
+            }
+        }
+    }
+}
