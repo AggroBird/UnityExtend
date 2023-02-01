774242214f20aeb50d9685536f3db0373d95e63b
AggroBird <jan@aggrobird.com>
2023-02-01 12:42:16 +0900

    Added support to clamped attribute for mixed values

--- Editor/ClampedAttributeDrawer.cs
+++ Editor/ClampedAttributeDrawer.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace AggroBird.UnityEngineExtend.Editor
 {
@@ -16,86 +16,101 @@ namespace AggroBird.UnityEngineExtend.Editor
             return EditorGUI.GetPropertyHeight(property, label, true);
         }
 
+
         private float Clamp(float n) => Mathf.Clamp(n, minFloat, maxFloat);
         private int Clamp(int n) => Mathf.Clamp(n, minInt, maxInt);
 
+        private void Clamp(SerializedProperty property)
+        {
+            if (!property.hasMultipleDifferentValues)
+            {
+                switch (property.propertyType)
+                {
+                    case SerializedPropertyType.Float:
+                        property.floatValue = Clamp(property.floatValue);
+                        break;
+                    case SerializedPropertyType.Integer:
+                        property.intValue = Clamp(property.intValue);
+                        break;
+                }
+            }
+        }
+        private void Clamp(SerializedProperty property, string name)
+        {
+            Clamp(property.FindPropertyRelative(name));
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            EditorGUI.BeginProperty(position, label, property);
+
             ClampedAttribute attr = (ClampedAttribute)attribute;
             minInt = attr.minInt;
             maxInt = attr.maxInt;
             minFloat = attr.minFloat;
             maxFloat = attr.maxFloat;
 
-            bool mixed = EditorGUI.showMixedValue;
-            EditorGUI.showMixedValue = property.hasMultipleDifferentValues;
-            EditorGUI.PropertyField(position, property, label, true);
-            if (!property.hasMultipleDifferentValues)
+            switch (property.propertyType)
             {
-                switch (property.propertyType)
+                case SerializedPropertyType.Integer:
                 {
-                    case SerializedPropertyType.Integer:
-                    {
-                        property.intValue = Clamp(property.intValue);
-                    }
-                    break;
-                    case SerializedPropertyType.Vector2Int:
-                    {
-                        Vector2Int v2 = property.vector2IntValue;
-                        v2.x = Clamp(v2.x);
-                        v2.y = Clamp(v2.y);
-                        property.vector2IntValue = v2;
-                    }
-                    break;
-                    case SerializedPropertyType.Vector3Int:
-                    {
-                        Vector3Int v3 = property.vector3IntValue;
-                        v3.x = Clamp(v3.x);
-                        v3.y = Clamp(v3.y);
-                        v3.z = Clamp(v3.z);
-                        property.vector3IntValue = v3;
-                    }
-                    break;
-
-                    case SerializedPropertyType.Float:
-                    {
-                        property.floatValue = Clamp(property.floatValue);
-                    }
-                    break;
-                    case SerializedPropertyType.Vector2:
-                    {
-                        Vector2 v2 = property.vector2Value;
-                        v2.x = Clamp(v2.x);
-                        v2.y = Clamp(v2.y);
-                        property.vector2Value = v2;
-                    }
-                    break;
-                    case SerializedPropertyType.Vector3:
-                    {
-                        Vector3 v3 = property.vector3Value;
-                        v3.x = Clamp(v3.x);
-                        v3.y = Clamp(v3.y);
-                        v3.z = Clamp(v3.z);
-                        property.vector3Value = v3;
-                    }
-                    break;
-                    case SerializedPropertyType.Vector4:
-                    {
-                        Vector4 v4 = property.vector4Value;
-                        v4.x = Clamp(v4.x);
-                        v4.y = Clamp(v4.y);
-                        v4.z = Clamp(v4.z);
-                        v4.w = Clamp(v4.w);
-                        property.vector4Value = v4;
-                    }
-                    break;
-
-                    default:
-                        EditorGUI.LabelField(position, label.text, "Invalid property type used for Clamped attribute");
-                        break;
+                    EditorGUI.PropertyField(position, property, label, true);
+                    Clamp(property);
                 }
+                break;
+                case SerializedPropertyType.Vector2Int:
+                {
+                    EditorGUI.PropertyField(position, property, label, true);
+                    Clamp(property, "x");
+                    Clamp(property, "y");
+                }
+                break;
+                case SerializedPropertyType.Vector3Int:
+                {
+                    EditorGUI.PropertyField(position, property, label, true);
+                    Clamp(property, "x");
+                    Clamp(property, "y");
+                    Clamp(property, "z");
+                }
+                break;
+
+                case SerializedPropertyType.Float:
+                {
+                    EditorGUI.PropertyField(position, property, label, true);
+                    Clamp(property);
+                }
+                break;
+                case SerializedPropertyType.Vector2:
+                {
+                    EditorGUI.PropertyField(position, property, label, true);
+                    Clamp(property, "x");
+                    Clamp(property, "y");
+                }
+                break;
+                case SerializedPropertyType.Vector3:
+                {
+                    EditorGUI.PropertyField(position, property, label, true);
+                    Clamp(property, "x");
+                    Clamp(property, "y");
+                    Clamp(property, "z");
+                }
+                break;
+                case SerializedPropertyType.Vector4:
+                {
+                    EditorGUI.PropertyField(position, property, label, true);
+                    Clamp(property, "x");
+                    Clamp(property, "y");
+                    Clamp(property, "z");
+                    Clamp(property, "w");
+                }
+                break;
+
+                default:
+                    EditorGUI.LabelField(position, label.text, "Invalid property type used for Clamped attribute");
+                    break;
             }
-            EditorGUI.showMixedValue = mixed;
+
+            EditorGUI.EndProperty();
         }
     }
 }
