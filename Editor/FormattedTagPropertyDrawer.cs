de50e5b5f7181e78be9523c1d58600fc1775cc4f
AggroBird <graaf093@outlook.com>
2023-02-11 17:27:58 +0900

    Added formatted tag attribute

---
+++ Editor/FormattedTagPropertyDrawer.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace AggroBird.UnityEngineExtend.Editor
+{
+    [CustomPropertyDrawer(typeof(FormattedTagAttribute))]
+    internal sealed class FormattedTagPropertyDrawer : PropertyDrawer
+    {
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+                EditorExtendUtility.FormatTag(property);
+            }
+            else
+            {
+                EditorGUI.LabelField(position, label.text, "FormattedTag attribute must be used with string");
+            }
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Runtime/FormattedTagAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AggroBird.UnityEngineExtend
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class FormattedTagAttribute : Attribute
+    {
+
+    }
+}
