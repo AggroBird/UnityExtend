97a21d8d45bda7511a6ecf7f2744c67140359565
AggroBird <jan@aggrobird.com>
2024-08-28 15:41:21 +0900

    Added override color swizzles

--- Runtime/Utility/Swizzle.cs
+++ Runtime/Utility/Swizzle.cs
@@ -123,6 +123,23 @@ namespace AggroBird.UnityExtend
             c.b = b;
         }
 
+        public static Color OverrideR(this Color c, float r)
+        {
+            return new Color(r, c.g, c.b, c.a);
+        }
+        public static Color OverrideG(this Color c, float g)
+        {
+            return new Color(c.r, g, c.b, c.a);
+        }
+        public static Color OverrideB(this Color c, float b)
+        {
+            return new Color(c.r, c.g, b, c.a);
+        }
+        public static Color OverrideA(this Color c, float a)
+        {
+            return new Color(c.r, c.g, c.b, a);
+        }
+
         public static Rect ToRect(this RectInt rect)
         {
             return new Rect(rect.x, rect.y, rect.width, rect.height);
