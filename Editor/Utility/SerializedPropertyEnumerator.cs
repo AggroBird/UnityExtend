27d3bae789f648ac75c3c5149bf3f3893f2fa7fc
AggroBird <jan@aggrobird.com>
2026-04-01 14:46:54 +0900

    Added recursive check

--- Editor/Utility/SerializedPropertyEnumerator.cs
+++ Editor/Utility/SerializedPropertyEnumerator.cs
@@ -8,35 +8,37 @@ namespace AggroBird.UnityExtend.Editor
         {
             private readonly SerializedProperty iter;
             private readonly SerializedProperty end;
+            private readonly bool recursive;
             private bool enterChildren;
 
-            public Enumerator(SerializedProperty iter, SerializedProperty end)
+            public Enumerator(SerializedProperty iter, SerializedProperty end, bool recursive)
             {
                 this.iter = iter;
                 this.end = end;
+                this.recursive = recursive;
                 enterChildren = true;
             }
 
-            public SerializedProperty Current => iter;
+            public readonly SerializedProperty Current => iter;
 
             public bool MoveNext()
             {
                 bool result = iter.NextVisible(enterChildren);
-                enterChildren = false;
+                enterChildren = recursive;
                 return result && (end == null || !SerializedProperty.EqualContents(iter, end));
             }
         }
 
         private readonly Enumerator enumerator;
 
-        public SerializedPropertyEnumerator(SerializedProperty serializedProperty)
+        public SerializedPropertyEnumerator(SerializedProperty serializedProperty, bool recursive = false)
         {
             serializedProperty = serializedProperty.Copy();
-            enumerator = new Enumerator(serializedProperty, serializedProperty.GetEndProperty());
+            enumerator = new Enumerator(serializedProperty, serializedProperty.GetEndProperty(), recursive);
         }
-        public SerializedPropertyEnumerator(SerializedObject serializedObject)
+        public SerializedPropertyEnumerator(SerializedObject serializedObject, bool recursive = false)
         {
-            enumerator = new Enumerator(serializedObject.GetIterator(), null);
+            enumerator = new Enumerator(serializedObject.GetIterator(), null, recursive);
         }
 
         public Enumerator GetEnumerator()
